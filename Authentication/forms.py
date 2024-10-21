a0894b8de2ff0f2753240a5ec211f4a90a6b07db
babygrim <bstetskiv@gmail.com>
2024-10-21 21:40:24 +0300

    added basic layout for login/sign up

--- Authentication/forms.py
+++ Authentication/forms.py
@@ -3,8 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(help_text='', label='Username')
+    password1 = forms.CharField(widget=forms.PasswordInput, help_text='', label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput, help_text='', label='Confirm Password')
+
     class Meta:
-        model = User 
+        model = User
         fields = ['username', 'password1', 'password2']
 
 class LoginForm(forms.Form):
